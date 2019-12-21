988b4954fccbde3a85cfce7f9ba87925d4d1c216
koralle <mkhd7930@gmail.com>
2019-12-21 12:33:03 +0900

    hogehoge

---
+++ src/pages/hobby.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout/Layout";
+
+const Hobby = () => {
+    return (
+        <Layout>
+            <h1>This is Hobby.</h1>
+        </Layout>
+    );
+}
+
+
+export default Hobby;
